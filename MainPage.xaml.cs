d418cbe7659a7682cdd15ac5ce8ec99c6871e40c
lomstfer <90508408+lomstfer@users.noreply.github.com>
2023-07-23 00:01:54 +0200

    permission scuff

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,14 +8,66 @@ namespace Metero;
 
 public partial class MainPage : ContentPage
 {
+    float gpsSpeed = 0;
+    float visualSpeed = 0;
+
+    float topSpeed = 0;
+
     public MainPage()
     {
         InitializeComponent();
-        TurnOnGPS(SpeedLabel);
+        TurnOnGPS();
+        Loop();
     }
 
-    public async void TurnOnGPS(Label label)
+    float Lerp(float firstFloat, float secondFloat, float by)
     {
+        return firstFloat * (1 - by) + secondFloat * by;
+    }
+
+    void OnResetTopSpeed(object o, EventArgs e)
+    {
+        topSpeed = 0;
+    }
+
+    private async void Loop()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        while (true)
+        {
+            float deltaTime = stopwatch.ElapsedMilliseconds / 1000f;
+            stopwatch.Restart();
+
+            visualSpeed = Lerp(visualSpeed, gpsSpeed, 1f - (float)Math.Pow(0.05, deltaTime));
+
+            SpeedLabel.Dispatcher.Dispatch(() =>
+            {
+                SpeedLabel.Text = visualSpeed.ToString("0");
+            });
+
+            if (gpsSpeed > topSpeed)
+            {
+                topSpeed = gpsSpeed;
+            }
+
+            TopSpeedLabel.Dispatcher.Dispatch(() =>
+            {
+                TopSpeedLabel.Text = topSpeed.ToString("0.0");
+            });
+
+            await Task.Delay(10);
+        }
+    }
+
+    private async void TurnOnGPS()
+    {
+        PermissionStatus status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+        if (status == PermissionStatus.Denied)
+        {
+            return;
+        }
+
         if (await CrossGeolocator.Current.StartListeningAsync(TimeSpan.FromSeconds(0.1), 1, true, new ListenerSettings
         {
             ActivityType = ActivityType.AutomotiveNavigation,
@@ -26,12 +78,10 @@ public partial class MainPage : ContentPage
         }))
         {
             CrossGeolocator.Current.PositionChanged += (s, e) => {
-                label.Dispatcher.Dispatch(() =>
-                {
-                    double metersPerSecond = e.Position.Speed;
-                    double kilometersPerHour = metersPerSecond * 3.6;
-                    label.Text = kilometersPerHour.ToString("0.0");
-                });
+                double metersPerSecond = e.Position.Speed;
+                double kilometersPerHour = metersPerSecond * 3.6;
+                //label.Text = kilometersPerHour.ToString("0.0");
+                gpsSpeed = (float)kilometersPerHour;
             };
         }
     }
